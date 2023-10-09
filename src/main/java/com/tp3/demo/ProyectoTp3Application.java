d7679eee3e83d244f8ad437b410a5612f7c3aa69
AugustusWasTaken <augus_greco@hotmail.com>
2023-10-09 20:45:01 -0300

    Initial commit

---
+++ src/main/java/com/tp3/demo/ProyectoTp3Application.java
@@ -0,0 +1,13 @@
+package com.tp3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTp3Application.class, args);
+	}
+
+}
