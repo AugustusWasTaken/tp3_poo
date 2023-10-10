572c823ca23a84aaa2dbd6d8494ad837e42d48ef
Juan Manuel Lopez <juanmlopez02@gmail.com>
2023-10-10 14:28:11 -0300

    TP4 - primer commit

---
+++ src/main/java/com/tp3/demo/service/UserServiceImp.java
@@ -0,0 +1,26 @@
+package com.tp3.demo.service;
+
+import com.tp3.demo.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImp implements UserService{
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails user = User.builder()
+                .username(username)
+                .password("contrasena")
+                .roles("USER")
+                .build();
+
+        return user;
+    }
+
+    @Override
+    public User create(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        return user;
+    }
+}
